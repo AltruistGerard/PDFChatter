98e9f12a24aa0ef61fb080ae9d6e064843dc9f3c
Gerard Fogarty <gerard@gerards-air.home>
2023-09-08 11:53:18 +0100

    Removed env and added secrets

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain.callbacks import get_openai_callback
 
 
 def main():
-    OPENAI_API_KEY= st.secrets[OPENAI_API_KEY]
+    OPENAI_API_KEY= st.secrets["OPENAI_API_KEY"]
     st.set_page_config(page_title="Altruistic Demo")
     st.header("PDF query tool 🔎📄")
     
